4c4ee23611c4e998ef220a91122da9e4cb3841af
Administrator <aiv700@udml14.cs.uregina.ca>
2018-11-09 18:45:43 -0600

    do not do hospital profile !!!!

--- bloodAssitant/ViewController.swift
+++ bloodAssitant/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    @IBOutlet weak var splashImage: UIImageView!
+    @IBOutlet weak var splashAppName: UILabel!
+    @IBOutlet weak var splashAppName2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
