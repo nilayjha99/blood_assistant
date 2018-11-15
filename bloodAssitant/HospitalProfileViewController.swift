9497d633fed0f55078902650fe35fefad0573952
Administrator <nhj123@udml10.cs.uregina.ca>
2018-11-15 00:19:14 -0600

    done: hospital profile UI

--- bloodAssitant/HospitalProfileViewController.swift
+++ bloodAssitant/HospitalProfileViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import MessageUI
 
-class HospitalProfileViewController: UIViewController {
+class HospitalProfileViewController: UIViewController, MFMailComposeViewControllerDelegate {
 
+    @IBOutlet weak var emailToSUpportTeam: UIButton!
+    @IBOutlet weak var hospitalProfileThumb: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        GeneralUtils.calculateRadusforCircle(viewObject: self.hospitalProfileThumb)
+     
+        GeneralUtils.makeRoundCorners(viewObject: self.emailToSUpportTeam, radius: 10)
     }
 
+    func configureMailController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self
+        
+        mailComposerVC.setToRecipients(["andrew@seemuapps.com"])
+        mailComposerVC.setSubject("Hello")
+        mailComposerVC.setMessageBody("How are you doing?", isHTML: false)
+//        mailComposerVC.setPreferredSendingEmailAddress(<#T##emailAddress: String##String#>)
+        return mailComposerVC
+    }
 
+    func showMailError() {
+        let sendMailErrorAlert = UIAlertController(title: "Could not send email", message: "Your device could not send email", preferredStyle: .alert)
+        let dismiss = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        sendMailErrorAlert.addAction(dismiss)
+        self.present(sendMailErrorAlert, animated: true, completion: nil)
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func openEmailApp(_ sender: UIButton) {
+        let mailComposeViewController = configureMailController()
+        if MFMailComposeViewController.canSendMail() {
+            self.present(mailComposeViewController, animated: true, completion: nil)
+        } else {
+            showMailError()
+        }
+    }
 }
 
